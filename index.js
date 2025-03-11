bc7fc33a67acb974450e351afff488d68e1d2027
Iesh <108469591+Iesht@users.noreply.github.com>
2025-03-11 15:31:52 +0500

    1, 2

--- index.js
+++ index.js
@@ -20,39 +20,6 @@ function addListeners() {
         });
 }
 
-/**
- * Блок плавно появляется из прозрачного.
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- */
-function fadeIn(element, duration) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.classList.remove('hide');
-    element.classList.add('show');
-}
-
-/**
- * Функция, передвигающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param translation — объект с полями x и y, обозначающими смещение блока
- */
-function move(element, duration, translation) {
-    element.style.transitionDuration = `${duration}ms`;
-    element.style.transform = getTransform(translation, null);
-}
-
-/**
- * Функция, увеличивающая/уменьшающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
- */
-function scale(element, duration, ratio) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.style.transform = getTransform(null, ratio);
-}
-
 function getTransform(translation, ratio) {
     const result = [];
     if (translation) {
@@ -63,3 +30,47 @@ function getTransform(translation, ratio) {
     }
     return result.join(' ');
 }
+
+function animaster() {
+  /**
+   * Блок плавно появляется из прозрачного.
+   * @param element — HTMLElement, который надо анимировать
+   * @param duration — Продолжительность анимации в миллисекундах
+   */
+  function fadeIn(element, duration) {
+    element.style.transitionDuration =  `${duration}ms`;
+    element.classList.remove('hide');
+    element.classList.add('show');
+  }
+
+  /**
+   * Функция, передвигающая элемент
+   * @param element — HTMLElement, который надо анимировать
+   * @param duration — Продолжительность анимации в миллисекундах
+   * @param translation — объект с полями x и y, обозначающими смещение блока
+   */
+  function move(element, duration, translation) {
+    element.style.transitionDuration = `${duration}ms`;
+    element.style.transform = getTransform(translation, null);
+  }
+
+  /**
+   * Функция, увеличивающая/уменьшающая элемент
+   * @param element — HTMLElement, который надо анимировать
+   * @param duration — Продолжительность анимации в миллисекундах
+   * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
+   */
+  function scale(element, duration, ratio) {
+    element.style.transitionDuration =  `${duration}ms`;
+    element.style.transform = getTransform(null, ratio);
+  }
+
+  return {
+    fadeIn, move, scale
+  };
+}
+
+const anim = animaster();
+const fadeIn = anim.fadeIn;
+const move = anim.move;
+const scale = anim.scale;
